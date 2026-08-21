9e8ccb8723aa48fb179c628d2b66124d9412002f
Gabriel Massaia <gabrielmassaia50@gmail.com>
2026-08-21 11:00:34 -0300

    feat: implementado user e company repository

---
+++ src/domain/repositories/ICompanyRepository.ts
@@ -0,0 +1,9 @@
+export interface ICompanyRepository {
+  create(data: {
+    name: string;
+    slug: string;
+    ownerId: string;
+  }): Promise<{ id: string; name: string; slug: string }>;
+
+  findByUserId(userId: string): Promise<{ id: string; name: string; slug: string } | null>;
+}
